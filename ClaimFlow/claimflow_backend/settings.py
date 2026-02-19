129f677d4ba0c991dde19f99972ba0a2a1b875b0
Abdul Mateen <abdulmateenaawan@gmail.com>
2026-02-19 10:57:54 +0500

    config setting of apps and models

--- ClaimFlow/claimflow_backend/settings.py
+++ ClaimFlow/claimflow_backend/settings.py
@@ -2,6 +2,8 @@
 
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'user',
+    'utils',
 ]
 
 MIDDLEWARE = [
@@ -111,3 +115,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "user.User"
